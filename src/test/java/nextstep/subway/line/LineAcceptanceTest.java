bab4e6ea5c78e462805080bf056d2f070015f368
joohokim <give928@gmail.com>
2022-05-23 23:55:23 +0900

    test: 지하철노선 생성 인수 테스트 작성

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -1,11 +1,24 @@
 package nextstep.subway.line;
 
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.BaseAcceptanceTest;
+import nextstep.subway.BaseRestAssured;
+import nextstep.subway.station.StationRestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DisplayName("지하철노선 관련 기능")
 class LineAcceptanceTest extends BaseAcceptanceTest {
+    private static final String LINE_URL = "/lines";
+    private static final String 신분당선 = "신분당선";
+
     /**
      * When 지하철 노선을 생성하면
      * Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다.
@@ -13,9 +26,24 @@ class LineAcceptanceTest extends BaseAcceptanceTest {
     @DisplayName("지하철노선을 생성한다.")
     @Test
     void createLine() {
+        // given
+        long 지하철역1_id = id추출(StationRestAssured.지하철역_생성_요청("지하철역1"));
+        long 지하철역2_id = id추출(StationRestAssured.지하철역_생성_요청("지하철역2"));
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", 신분당선);
+        params.put("color", "bg-red-600");
+        params.put("upStationId", 지하철역1_id);
+        params.put("downStationId", 지하철역2_id);
+        params.put("distance", 10);
+
         // when
+        BaseRestAssured.post(LINE_URL, params);
 
         // then
+        ExtractableResponse<Response> response = BaseRestAssured.get(LINE_URL);
+        List<String> lineNames = response.jsonPath().getList("name", String.class);
+        assertThat(lineNames).containsExactly(신분당선);
     }
 
     /**
