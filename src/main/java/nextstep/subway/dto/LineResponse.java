5f845d50856dd4e35378919dc8656e9998fd2a7a
joohokim <give928@gmail.com>
2022-05-24 23:10:38 +0900

    refactor: 지하철노선 응답 dto 생성자 접근제한자 public -> private 변경

--- src/main/java/nextstep/subway/dto/LineResponse.java
+++ src/main/java/nextstep/subway/dto/LineResponse.java
@@ -11,7 +11,7 @@ public class LineResponse {
     private final String color;
     private final List<StationResponse> stations;
 
-    public LineResponse(Long id, String name, String color, List<StationResponse> stations) {
+    private LineResponse(Long id, String name, String color, List<StationResponse> stations) {
         this.id = id;
         this.name = name;
         this.color = color;
